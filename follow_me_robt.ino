0c0e6d09a01519d1beaf78b1c45ff9d740cd59d9
amithlal200047 <82321175+amithlal200047@users.noreply.github.com>
2021-08-04 00:01:34 +0530

    Update follow_me_robt.ino

--- follow_me_robt.ino
+++ follow_me_robt.ino
@@ -1,3 +1,5 @@
+ #define Enable1 5  // Enable Pin for motor 1
+#define Enable2 3  // Enable Pin for motor 2
 #define trigpin 13
 #define ecopin 12
 #define motorleft_c 11
@@ -6,13 +8,16 @@
 #define motorright_a 8
 #define ir_left 7
 #define ir_right 6
-#define vibrater 5
+#define mini1 4
+#define mini2 2
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
+  pinMode(Enable1, OUTPUT);
+  pinMode(Enable2, OUTPUT);
   pinMode(trigpin,OUTPUT);
   pinMode(ecopin,INPUT);
   pinMode(motorleft_c,OUTPUT);
@@ -21,34 +26,89 @@ void setup() {
   pinMode(motorright_a,OUTPUT);
   pinMode(ir_left,INPUT);
   pinMode(ir_right,INPUT);
-  pinMode(vibrater,INPUT);
+  pinMode(mini1,INPUT);
+  pinMode(mini2,INPUT);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Helo..");
 }
 
 void loop() {
+  
+  analogWrite(Enable1, 90);
+  analogWrite(Enable2, 90);
+  if(digitalRead(mini2)==HIGH){
+    lcd.setCursor(0,0);
+    lcd.print("LOCKED      ");
+    Serial.println("Recived locked");
+    }
+  else if(digitalRead(mini1)==LOW){
+    
+    lcd.setCursor(0,0);
+    lcd.print("FOLLOW MODE ");
+    Serial.println("Recived follow mode");
+    }
+  else if(digitalRead(mini1)==HIGH){
+    
+    lcd.setCursor(0,0);
+    lcd.print("CONTROL MODE");
+    Serial.println("Recived control mode");
+    
+    }
+    
+  if(digitalRead(mini1)==LOW && digitalRead(mini2)==LOW){
   digitalWrite(trigpin,HIGH);
   delayMicroseconds(10);
   digitalWrite(trigpin,LOW);
   long tim=pulseIn(ecopin,HIGH)/2;
   long distance=tim*0.034;
-  if(distance<50){
-    if(ir_left==HIGH){
+  //Serial.println(distance);
+  if(distance<20){
+    if(digitalRead(ir_left)==HIGH && digitalRead(ir_right)==LOW){
         digitalWrite(motorleft_c,HIGH);
         digitalWrite(motorright_a,HIGH);
+        digitalWrite(motorleft_a,LOW);
+        digitalWrite(motorright_c,LOW);
+        //Serial.println("Left");
       }
-    else if(ir_right==HIGH){
+    else if(digitalRead(ir_right)==HIGH && digitalRead(ir_left)==LOW){
         digitalWrite(motorleft_a,HIGH);
         digitalWrite(motorright_c,HIGH);
+        digitalWrite(motorleft_c,LOW);
+        digitalWrite(motorright_a,LOW);
+        //Serial.println("Right");
       }
-    else if(ir_left==HIGH){
+    else{
         digitalWrite(motorleft_a,HIGH);
         digitalWrite(motorright_a,HIGH);
+        digitalWrite(motorleft_c,LOW);
+        digitalWrite(motorright_c,LOW);
+        //Serial.println("Forward");
       }
       
+      
   }
+  else if(digitalRead(ir_left)==HIGH && digitalRead(ir_right)==LOW){
+        digitalWrite(motorleft_c,HIGH);
+        digitalWrite(motorright_a,HIGH);
+        digitalWrite(motorleft_a,LOW);
+        digitalWrite(motorright_c,LOW);
+        //Serial.println("Left");
+      }
+  else if(digitalRead(ir_right)==HIGH && digitalRead(ir_left)==LOW){
+        digitalWrite(motorleft_a,HIGH);
+        digitalWrite(motorright_c,HIGH);
+        digitalWrite(motorleft_c,LOW);
+        digitalWrite(motorright_a,LOW);
+        //Serial.println("Right");
+      }
+  
   else{
     digitalWrite(motorleft_c,LOW);
     digitalWrite(motorleft_a,LOW);
     digitalWrite(motorright_c,LOW);
     digitalWrite(motorright_a,LOW);
+    //Serial.println("Stopped");
+    }
   }
 }
